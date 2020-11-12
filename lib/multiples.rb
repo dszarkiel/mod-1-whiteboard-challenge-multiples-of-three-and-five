51a326a7bd5ee52b759f6e43e70ea2d932ffc8c9
dszarkiel <d.szarkiel@gmail.com>
2020-11-12 09:49:23 -0600

    commit tree 43899df8f1f24a39c3b4e4a8884139e199e36428

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
-# Enter your procedural solution here!
+
+require 'pry'
+
+def collect_multiples(limit)
+numbers = 1...limit
+numbers.select do |num|
+    num % 3 == 0 || num % 5 == 0
+    end
+end
+
+def sum_multiples(limit)
+multiples = collect_multiples(limit)
+multiples.sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+class Multiples 
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+    numbers = 1...@limit
+    numbers.select do |num|
+        num % 3 == 0 || num % 5 == 0
+        end
+    end
+
+    def sum_multiples
+        numbers = collect_multiples
+        numbers.sum
+    end     
+end
